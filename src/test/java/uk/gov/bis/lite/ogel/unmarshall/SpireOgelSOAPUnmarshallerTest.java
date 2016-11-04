8ed91731a85eb58bbd2fb700fb55bafd28d4cc7d
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2016-11-04 17:07:29 +0000

    Updated OGEL Types Response test source xml, modified tests to accept the new data

--- src/test/java/uk/gov/bis/lite/ogel/unmarshall/SpireOgelSOAPUnmarshallerTest.java
+++ src/test/java/uk/gov/bis/lite/ogel/unmarshall/SpireOgelSOAPUnmarshallerTest.java
@@ -31,6 +31,8 @@ public class SpireOgelSOAPUnmarshallerTest {
   private String OGL0 = "OGL0";
   private String OGL1 = "OGL1";
   private String OGL2 = "OGL2";
+  private String OGL3 = "OGL3";
+  private String OGL4 = "OGL4";
 
   private String EXCLUDED = "[EXCLUDED]";
   private String INCLUDED = "[INCLUDED]";
@@ -39,7 +41,7 @@ public class SpireOgelSOAPUnmarshallerTest {
   public void setUp()  throws ParserConfigurationException, IOException, SAXException, XPathExpressionException {
     DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
     DocumentBuilder builder = factory.newDocumentBuilder();
-    Document document = builder.parse(ClassLoader.getSystemResourceAsStream("OgelTypes012Response.xml"));
+    Document document = builder.parse(ClassLoader.getSystemResourceAsStream("OgelTypes01234Response.xml"));
     XPath xPath = XPathFactory.newInstance().newXPath();
     NodeList nodeList = (NodeList) xPath.compile("//OGEL_TYPES_LIST").evaluate(document, XPathConstants.NODESET);
     ogels = new SpireOgelSOAPUnmarshaller().parseSoapBody(nodeList, xPath);
@@ -48,8 +50,8 @@ public class SpireOgelSOAPUnmarshallerTest {
   @Test
   public void testOgelsList() {
     assertTrue(!ogels.isEmpty());
-    assertEquals(3, ogels.size());
-    assertThat(ogels).extracting(SpireOgel::getId).containsOnly(OGL0, OGL1, OGL2);
+    assertEquals(5, ogels.size());
+    assertThat(ogels).extracting(SpireOgel::getId).containsOnly(OGL0, OGL1, OGL2, OGL3, OGL4);
   }
 
   @Test
@@ -71,15 +73,35 @@ public class SpireOgelSOAPUnmarshallerTest {
         .extracting(cons -> cons.get(0)).extracting("countryStatus").hasToString(EXCLUDED);
     assertThat(ogels).filteredOn("id", OGL2).extracting(SpireOgel::getOgelConditions)
         .extracting(cons -> cons.get(0)).flatExtracting(OgelCondition::getCountries).asList().hasSize(62);
+
+    assertThat(ogels).filteredOn("id", OGL3).extracting(SpireOgel::getOgelConditions).hasSize(1);
+    assertThat(ogels).filteredOn("id", OGL3).extracting(SpireOgel::getOgelConditions)
+        .extracting(cons -> cons.get(0)).extracting("countryStatus").hasToString(INCLUDED);
+    assertThat(ogels).filteredOn("id", OGL3).extracting(SpireOgel::getOgelConditions)
+        .extracting(cons -> cons.get(0)).flatExtracting(OgelCondition::getCountries).asList().hasSize(44);
+
+    assertThat(ogels).filteredOn("id", OGL4).extracting(SpireOgel::getOgelConditions).hasSize(1);
+    assertThat(ogels).filteredOn("id", OGL4).extracting(SpireOgel::getOgelConditions)
+        .extracting(cons -> cons.get(0)).extracting("countryStatus").hasToString(EXCLUDED);
+    assertThat(ogels).filteredOn("id", OGL4).extracting(SpireOgel::getOgelConditions)
+        .extracting(cons -> cons.get(0)).flatExtracting(OgelCondition::getCountries).asList().hasSize(41);
   }
 
   @Test
   public void testRatings() {
     assertThat(ogels).filteredOn("id", OGL0).extracting(SpireOgel::getOgelConditions)
         .extracting(cons -> cons.get(0)).flatExtracting(OgelCondition::getRatingList).asList().hasSize(10);
+
     assertThat(ogels).filteredOn("id", OGL1).extracting(SpireOgel::getOgelConditions)
         .extracting(cons -> cons.get(0)).flatExtracting(OgelCondition::getRatingList).asList().hasSize(79);
+
     assertThat(ogels).filteredOn("id", OGL2).extracting(SpireOgel::getOgelConditions)
         .extracting(cons -> cons.get(0)).flatExtracting(OgelCondition::getRatingList).asList().hasSize(284);
+
+    assertThat(ogels).filteredOn("id", OGL3).extracting(SpireOgel::getOgelConditions)
+        .extracting(cons -> cons.get(0)).flatExtracting(OgelCondition::getRatingList).asList().hasSize(309);
+
+    assertThat(ogels).filteredOn("id", OGL4).extracting(SpireOgel::getOgelConditions)
+        .extracting(cons -> cons.get(0)).flatExtracting(OgelCondition::getRatingList).asList().hasSize(80);
   }
 }
