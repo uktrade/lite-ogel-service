78b55763f00a0ec26b92ededf63d5d671d364e35
Pete Nattress <pete.nattress@fivium.co.uk>
2017-04-24 18:34:33 +0100

    Rename test class

--- src/test/java/uk/gov/bis/lite/ogel/service/ApplicableOgelServiceImplTest.java
+++ src/test/java/uk/gov/bis/lite/ogel/service/ApplicableOgelServiceImplTest.java
@@ -16,10 +16,10 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class ApplicableOgelServiceTest {
+public class ApplicableOgelServiceImplTest {
 
   private SpireOgelService mockedSpireOgelService = mock(SpireOgelService.class);
-  private ApplicableOgelService applicableOgelService;
+  private ApplicableOgelServiceImpl applicableOgelService;
 
   @Before
   public void setUp() {
