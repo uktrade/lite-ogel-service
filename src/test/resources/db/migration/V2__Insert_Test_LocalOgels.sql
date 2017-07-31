5776516eabfc6c4de2f1b2f5134075d4a9540152
Aafreen Ansari <aafreen.ansari@digital.trade.gov.uk>
2017-07-31 09:38:03 +0100

    LITE-663: test data

--- src/test/resources/db/migration/V2__Insert_Test_LocalOgels.sql
+++ src/test/resources/db/migration/V2__Insert_Test_LocalOgels.sql
@@ -1,6 +1,6 @@
 INSERT INTO LOCAL_OGEL (ID, NAME, CANLIST, CANTLIST, MUSTLIST, HOWTOUSELIST)
-VALUES ('OGLX', 'NameOGLX', 'null', 'null', 'null', 'null');
+VALUES ('OGLX', 'NameOGLX', '["CanList for OGLX"]', '["CantList for OGLX"]', 'null', 'null');
 INSERT INTO LOCAL_OGEL (ID, NAME, CANLIST, CANTLIST, MUSTLIST, HOWTOUSELIST)
-VALUES ('OGLY', 'NameOGLY', 'null', 'null', 'null', 'null');
+VALUES ('OGLY', 'NameOGLY', '["CanList for OGLY"]', '["CantList for OGLY"]', 'null', 'null');
 INSERT INTO LOCAL_OGEL (ID, NAME, CANLIST, CANTLIST, MUSTLIST, HOWTOUSELIST)
-VALUES ('OGLZ', 'NameOGLZ', 'null', 'null', 'null', 'null');
+VALUES ('OGLZ', 'NameOGLZ', '["CanList for OGLZ"]', '["CantList for OGLZ"]', 'null', 'null');
