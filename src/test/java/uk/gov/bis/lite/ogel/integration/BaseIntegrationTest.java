76233cf24d2266c40f48d8f28289525f4e866e4b
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-08-21 14:32:00 +0100

    Use WireMockClassRule in BaseIntegrationTest

--- src/test/java/uk/gov/bis/lite/ogel/integration/BaseIntegrationTest.java
+++ src/test/java/uk/gov/bis/lite/ogel/integration/BaseIntegrationTest.java
@@ -9,11 +9,14 @@ import static io.dropwizard.testing.ResourceHelpers.resourceFilePath;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.awaitility.Awaitility.await;
 
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.flywaydb.core.Flyway;
 import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -24,19 +27,30 @@ import uk.gov.bis.lite.ogel.config.MainApplicationConfiguration;
 public class BaseIntegrationTest {
 
   @ClassRule
-  public static final WireMockRule wireMockRule = new WireMockRule(9000);
+  public static final WireMockClassRule wireMockRule  = new WireMockClassRule(9000);
+
+  @Rule
+  public WireMockClassRule instanceRule = wireMockRule;
 
   @Rule
   public final DropwizardAppRule<MainApplicationConfiguration> RULE =
       new DropwizardAppRule<>(OgelApplication.class, resourceFilePath("service-test.yaml"));
 
-  @BeforeClass
-  public static void setUpMock() {
+  @Before
+  public void setUpMock() {
     stubFor(post(urlEqualTo("/spire/fox/ispire/SPIRE_OGEL_TYPES"))
         .willReturn(aResponse()
             .withStatus(200)
             .withHeader("Content-Type", "text/xml")
             .withBody(fixture("fixture/integration/spire/getAllOgelsResponse.xml"))));
+  }
+
+  @Before
+  public void setupDatabase() {
+    DataSourceFactory f = RULE.getConfiguration().getDataSourceFactory();
+    Flyway flyway = new Flyway();
+    flyway.setDataSource(f.getUrl(), f.getUser(), f.getPassword());
+    flyway.migrate();
   }
 
   @Before
@@ -46,13 +60,5 @@ public class BaseIntegrationTest {
         .request()
         .get()
         .getStatus() == 200);
-  }
-
-  @Before
-  public void setupDatabase() {
-    DataSourceFactory f = RULE.getConfiguration().getDataSourceFactory();
-    Flyway flyway = new Flyway();
-    flyway.setDataSource(f.getUrl(), f.getUser(), f.getPassword());
-    flyway.migrate();
   }
 }
