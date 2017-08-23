06bc5e9d8d4d745c05070ed75d356f3bcf0daf1f
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-08-23 08:54:02 +0100

    Converted WireMock tests to use dynamic port allocation

--- src/test/java/uk/gov/bis/lite/ogel/integration/BaseIntegrationTest.java
+++ src/test/java/uk/gov/bis/lite/ogel/integration/BaseIntegrationTest.java
@@ -1,9 +1,11 @@
 package uk.gov.bis.lite.ogel.integration;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static io.dropwizard.testing.FixtureHelpers.fixture;
 import static io.dropwizard.testing.ResourceHelpers.resourceFilePath;
 import static java.util.concurrent.TimeUnit.SECONDS;
@@ -11,48 +13,65 @@ import static org.awaitility.Awaitility.await;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.testing.ConfigOverride;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.flywaydb.core.Flyway;
 import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import uk.gov.bis.lite.ogel.OgelApplication;
 import uk.gov.bis.lite.ogel.config.MainApplicationConfiguration;
 
 public class BaseIntegrationTest {
+  /**
+   * Manually starting WireMockRule and DropwizardAppRule as WireMockRule is configured to use dynamic port allocation due
+   * to a bug with WireMocks' Jetty configuration.
+   * TODO convert to @ClassRule WireMockClassRule when https://github.com/tomakehurst/wiremock/issues/97 is resolved.
+   */
+  public WireMockRule wireMockRule;
 
-  @ClassRule
-  public static final WireMockRule wireMockRule = new WireMockRule(9000);
+  public DropwizardAppRule<MainApplicationConfiguration> dwAppRule;
 
-  @Rule
-  public final DropwizardAppRule<MainApplicationConfiguration> RULE =
-      new DropwizardAppRule<>(OgelApplication.class, resourceFilePath("service-test.yaml"));
+  @Before
+  public void setUp() {
+    // WireMock setup
+    wireMockRule = new WireMockRule(options().dynamicPort());
+    wireMockRule.start();
 
-  @BeforeClass
-  public static void setUpMock() {
+    // configures stubFor to use allocated port
+    configureFor("localhost",  wireMockRule.port());
+
     stubFor(post(urlEqualTo("/spire/fox/ispire/SPIRE_OGEL_TYPES"))
         .willReturn(aResponse()
             .withStatus(200)
             .withHeader("Content-Type", "text/xml")
             .withBody(fixture("fixture/integration/spire/getAllOgelsResponse.xml"))));
-  }
 
-  @Before
-  public void awaitSpireOgelCacheLoad() {
+    // Dropwizard setup overriding
+    dwAppRule = new DropwizardAppRule<>(OgelApplication.class, resourceFilePath("service-test.yaml"),
+        ConfigOverride.config("controlCodeServiceUrl", "http://localhost:" +  wireMockRule.port() + "/"),
+        ConfigOverride.config("spireClientUrl", "http://localhost:" +  wireMockRule.port() + "/spire/fox/ispire/"));
+    dwAppRule.getTestSupport().before();
+
+    // Setup database
+    DataSourceFactory f = dwAppRule.getConfiguration().getDataSourceFactory();
+    Flyway flyway = new Flyway();
+    flyway.setDataSource(f.getUrl(), f.getUser(), f.getPassword());
+    flyway.migrate();
+
+    // await Spire OGEL cache load
     await().with().pollInterval(1, SECONDS).atMost(10, SECONDS).until(() -> JerseyClientBuilder.createClient()
-        .target("http://localhost:"+RULE.getAdminPort()+"/ready")
+        .target("http://localhost:"+ dwAppRule.getAdminPort()+"/ready")
         .request()
         .get()
         .getStatus() == 200);
   }
 
-  @Before
-  public void setupDatabase() {
-    DataSourceFactory f = RULE.getConfiguration().getDataSourceFactory();
-    Flyway flyway = new Flyway();
-    flyway.setDataSource(f.getUrl(), f.getUser(), f.getPassword());
-    flyway.migrate();
+  @After
+  public void tearDown() throws Exception {
+    wireMockRule.stop();
+    dwAppRule.getTestSupport().after();
+    wireMockRule = null;
+    dwAppRule = null;
   }
 }
