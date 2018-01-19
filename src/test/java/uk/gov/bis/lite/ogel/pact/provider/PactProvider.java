ae1dbb37335c20ba553014de4f8d44fad6e2ee8a
Pete Nattress <pete.nattress@fivium.co.uk>
2018-01-19 16:20:28 +0000

    DIT Jenkins migration

--- src/test/java/uk/gov/bis/lite/ogel/pact/provider/PactProvider.java
+++ src/test/java/uk/gov/bis/lite/ogel/pact/provider/PactProvider.java
@@ -20,7 +20,7 @@ import uk.gov.bis.lite.ogel.service.SpireOgelServiceMock;
 
 @RunWith(PactRunner.class)
 @Provider("lite-ogel-service")
-@PactBroker(host = "pact-broker.mgmt.licensing.service.trade.gov.uk.test", port = "80")
+@PactBroker(host = "pact-broker.ci.uktrade.io", port = "80")
 public class PactProvider {
 
   @ClassRule
