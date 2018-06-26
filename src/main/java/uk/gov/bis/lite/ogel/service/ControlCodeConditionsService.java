63f1b656ad8e6b5cb8b65659914b2c1bfdf71f00
Aafreen Ansari <aafreen.ansari@digital.trade.gov.uk>
2018-06-26 13:18:40 +0100

    PF-253: Removed ControlCodeConditionsResource,ControlCodeClient and dependant classes, tests and resources

--- src/main/java/uk/gov/bis/lite/ogel/service/ControlCodeConditionsService.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.bis.lite.ogel.service;
-
-import uk.gov.bis.lite.ogel.api.view.ControlCodeConditionFullView;
-
-import java.util.Optional;
-
-public interface ControlCodeConditionsService {
-  Optional<ControlCodeConditionFullView> findControlCodeConditions(String ogelID, String controlCode);
-}
