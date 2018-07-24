582e76d090b4e77b55b33994d1e55211020da359
Dan Haynes <djm.haynes@gmail.com>
2018-07-24 13:45:12 +0100

    PF-394 Added back missing path notation

--- src/main/java/uk/gov/bis/lite/ogel/resource/AdminResource.java
+++ src/main/java/uk/gov/bis/lite/ogel/resource/AdminResource.java
@@ -24,6 +24,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+@Path("/admin")
 @Produces(MediaType.APPLICATION_JSON)
 public class AdminResource {
 
